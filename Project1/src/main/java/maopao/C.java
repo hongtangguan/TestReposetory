700f0e45f053346a0b0a6ec9e1d10d849637d66d
zhenggaosheng <742664726@qq.com>
2018-10-31 20:00:07 +0800

    测试提交....删除不用的类

--- Project1/src/main/java/maopao/C.java
+++
@@ -1,9 +0,0 @@
-package maopao;
-
-/**
- * @Auther: ZGS
- * @Date: 2018/10/31 17:10
- * @Description:
- */
-public class C {
-}
--- Project1/src/main/java/maopao/TEST.java
+++
@@ -1,9 +0,0 @@
-package maopao;
-
-/**
- * @Auther: ZGS
- * @Date: 2018/10/31 17:06
- * @Description:
- */
-public class TEST {
-}
